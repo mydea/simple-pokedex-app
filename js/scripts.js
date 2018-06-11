d6bbfe559d8ebdc50c2581f2d36c891e307e70b9
Francesco Novy <francesco@cropster.com>
2018-06-11 18:31:28 +0200

    A2.6 - Load data with fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var pokemonRepository = (function () {
     var items = [];
+    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function addSingle(item) {
         if (validate(item)) {
@@ -39,11 +40,62 @@ var pokemonRepository = (function () {
         });
     }
 
+    function loadList() {
+        showLoadingMessage();
+        return fetch(apiUrl).then(function (response) {
+            // response.ok checks if the status === 200
+            if (!response.ok) {
+                return Promise.reject('Error loading data'); // This will end in the .catch block
+            }
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                var pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+            hideLoadingMessage();
+        }).catch(function (e) {
+            console.error(e);
+            hideLoadingMessage();
+        })
+    }
+
+    function loadDetails(item) {
+        var url = item.detailsUrl;
+        var imageUrl = item.imageUrl;
+
+        // If an image is already loaded, we don't need to fetch it again
+        // We still want to return a Promise, as otherwise the .then will not work
+        if (imageUrl) {
+            return Promise.resolve(); // This returns a new Promise that resolves immediately
+        }
+
+        showLoadingMessage();
+        return fetch(url).then(function (response) {
+            if (!response.ok) {
+                return Promise.reject('Error loading data'); // This will end in the .catch block
+            }
+            return response.json();
+        }).then(function (details) {
+            // Now we add the details to the item
+            item.imageUrl = details.sprites.front_default;
+            hideLoadingMessage();
+        }).catch(function (e) {
+            console.error(e);
+            hideLoadingMessage();
+        });
+    }
+
     return {
         add: add,
         getAll: getAll,
         remove: remove,
-        search: search
+        search: search,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
 })();
 
@@ -56,8 +108,50 @@ function addListItem(item) {
     listItem.appendChild(title);
 
     container.appendChild(listItem);
+
+    listItem.addEventListener('click', function () {
+        showDetails(item, listItem);
+    });
 }
 
-addListItem({ name: 'Pikachu' });
-addListItem({ name: 'Bulbasaur' });
-addListItem({ name: 'Charmander' });
+function showDetails(item, listItem) {
+    var listItemDetails = listItem.querySelector('.details');
+
+    // If details already exist, remove them
+    if (listItemDetails) {
+        listItem.removeChild(listItemDetails);
+    } else {
+        // Else, first load the details, then add the element
+        pokemonRepository.loadDetails(item).then(function () {
+            var imageUrl = item.imageUrl;
+
+            listItemDetails = document.createElement('img');
+            listItemDetails.setAttribute('src', imageUrl);
+            listItemDetails.classList.add('details');
+            listItem.appendChild(listItemDetails);
+        });
+    }
+}
+
+function showLoadingMessage() {
+    var container = document.querySelector('main');
+
+    var loadingMessage = document.createElement('div');
+    loadingMessage.classList.add('loading-message');
+    loadingMessage.innerText = 'Loading...';
+
+    container.insertBefore(loadingMessage, container.firstElementChild);
+}
+
+function hideLoadingMessage() {
+    var loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.parentElement.removeChild(loadingMessage);
+}
+
+pokemonRepository.loadList().then(function () {
+    // Now the data is loaded!
+    var allPokemon = pokemonRepository.getAll();
+    allPokemon.forEach(function (item) {
+        addListItem(item);
+    });
+});
