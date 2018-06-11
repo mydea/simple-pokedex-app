3a584f9123884ade39821685330dc78ed944e808
Francesco Novy <francesco@cropster.com>
2018-06-11 18:14:03 +0200

    A2.4 - Add data repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,48 @@
-alert("Hello world");
+var pokemonRepository = (function () {
+    var items = [];
+
+    function addSingle(item) {
+        if (validate(item)) {
+            items.push(item);
+        } else {
+            console.error('Error when validating item', item);
+        }
+    }
+
+    function add(item) {
+        if (Array.isArray(item)) {
+            items.forEach(addSingle);
+        } else {
+            addSingle(item);
+        }
+    }
+
+    function validate(item) {
+        return typeof item === 'object' && item.name;
+    }
+
+    function getAll() {
+        return items;
+    }
+
+    function remove(item) {
+        var itemPos = items.indexOf(item);
+        if (itemPos > -1) {
+            items.splice(itemPos, 1);
+        }
+    }
+
+    function search(str) {
+        str = str.toLowerCase(); // We want to compare lower case strings
+        return items.filter(function (item) {
+            return item.name.toLowerCase().indexOf(str) > -1;
+        });
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+        remove: remove,
+        search: search
+    };
+})();
